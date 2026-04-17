8b8422c20cf2023a4b6ce4f2371c535b08a513cb
SMARK2022 <3222301373@qq.com>
2026-04-17 12:03:07 +0800

    fix: 更新模块搜索功能，支持显示模块标题和别名

--- src/main/java/meteordevelopment/meteorclient/gui/screens/ModulesScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/ModulesScreen.java
@@ -19,7 +19,9 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.misc.NbtUtils;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Items;
+import net.minecraft.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +29,12 @@ import java.util.Set;
 
 import static meteordevelopment.meteorclient.utils.Utils.getWindowHeight;
 import static meteordevelopment.meteorclient.utils.Utils.getWindowWidth;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class ModulesScreen extends TabScreen {
     private WCategoryController controller;
+    private WWindow searchWindow;
+    private WTextBox searchTextBox;
 
     public ModulesScreen(GuiTheme theme) {
         super(theme, Tabs.get().getFirst());
@@ -80,30 +85,30 @@ public class ModulesScreen extends TabScreen {
     protected void createSearchW(WContainer w, String text) {
         if (!text.isEmpty()) {
             // Titles
-            Set<Module> modules = Modules.get().searchTitles(text);
+            List<Pair<Module, String>> modules = Modules.get().searchTitles(text);
 
             if (!modules.isEmpty()) {
                 WSection section = w.add(theme.section("Modules")).expandX().widget();
                 section.spacing = 0;
 
                 int count = 0;
-                for (Module module : modules) {
+                for (Pair<Module, String> p : modules) {
                     if (count >= Config.get().moduleSearchCount.get() || count >= modules.size()) break;
-                    section.add(theme.module(module)).expandX();
+                    section.add(theme.module(p.getLeft(), p.getRight())).expandX();
                     count++;
                 }
             }
 
             // Settings
-            modules = Modules.get().searchSettingTitles(text);
+            Set<Module> settings = Modules.get().searchSettingTitles(text);
 
-            if (!modules.isEmpty()) {
+            if (!settings.isEmpty()) {
                 WSection section = w.add(theme.section("Settings")).expandX().widget();
                 section.spacing = 0;
 
                 int count = 0;
-                for (Module module : modules) {
-                    if (count >= Config.get().moduleSearchCount.get() || count >= modules.size()) break;
+                for (Module module : settings) {
+                    if (count >= Config.get().moduleSearchCount.get() || count >= settings.size()) break;
                     section.add(theme.module(module)).expandX();
                     count++;
                 }
@@ -114,6 +119,7 @@ public class ModulesScreen extends TabScreen {
     protected WWindow createSearch(WContainer c) {
         WWindow w = theme.window("Search");
         w.id = "search";
+        searchWindow = w;
 
         if (theme.categoryIcons()) {
             w.beforeHeaderInit = wContainer -> wContainer.add(theme.item(Items.COMPASS.getDefaultStack())).pad(2);
@@ -128,6 +134,7 @@ public class ModulesScreen extends TabScreen {
 
         WTextBox text = w.add(theme.textBox("")).minWidth(140).expandX().widget();
         text.setFocused(true);
+        searchTextBox = text;
         text.action = () -> {
             l.clear();
             createSearchW(l, text.get());
@@ -137,6 +144,25 @@ public class ModulesScreen extends TabScreen {
         createSearchW(l, text.get());
 
         return w;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (locked) return false;
+
+        boolean cntrl = MinecraftClient.IS_SYSTEM_MAC ? modifiers == GLFW_MOD_SUPER : modifiers == GLFW_MOD_CONTROL;
+
+        if (cntrl && keyCode == GLFW_KEY_F) {
+            if (searchWindow != null) searchWindow.setExpanded(true);
+            if (searchTextBox != null) {
+                searchTextBox.setFocused(true);
+                searchTextBox.setCursorMax();
+            }
+
+            return true;
+        }
+
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 
     // Favorites
